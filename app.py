4c86a7101b1cc68ecd86f63a57a9d893e44a6a62
inatoihs <sabanna.taihei@gmail.com>
2024-08-10 12:21:21 +0900

    学生一覧画面の高速化

--- app.py
+++ app.py
@@ -112,8 +112,10 @@ def signin():
         return redirect(url_for("before_delete"))
     return render_template("Xlogin.html", form=form)
 
+
 # tweets.db memo
 # id, text, date, likes, should_delete, user_id, score,
+
 
 @app.route("/before_delete/")
 def before_delete():
@@ -147,7 +149,9 @@ def before_delete():
             print("res", res)
         print(response.choices[0].message.content)
         score_list.append(response.choices[0].message.content)
-        cursor.execute("UPDATE tweets SET score = ? WHERE text = ?", (int(res), tweet[0]))
+        cursor.execute(
+            "UPDATE tweets SET score = ? WHERE text = ?", (int(res), tweet[0])
+        )
 
     db.commit()
     db.close()
@@ -178,7 +182,7 @@ def after_delete():
         if tweet[1] == 0:
             score_list.append(tweet[2])
 
-    #score_list = [int(score) for score in score_list]
+    # score_list = [int(score) for score in score_list]
     total_score = sum(score_list) / len(score_list)
     total_score = total_score * 200
     # 　切り捨て
@@ -350,7 +354,7 @@ def student_list():
             "grade": "学部3",
             "industry": "IT",
             "twitter_account": "@daigaku_saikou",
-            "summary": "サッカーが好きな学生です。海外に行くことが好きで、英語が得意です。",
+            "summary": "この学生は、サッカーと英語学習に情熱を注いでいるアクティブな人物です。チームプレーの重要性を理解し、連携を重視している一方で、サッカーの練習がハードで体力的な限界を感じることもあるようです。また、異文化に触れることで楽しみを感じており、英語のスキル向上にも積極的です。英語の発音に不安を抱えつつも、学びを続け、海外のニュースもスムーズに読めるようになったことを喜んでいます。試合の結果に一喜一憂しつつも、チームの成長を実感し、次の試合への期待を持っています。",
             "image": "https://as2.ftcdn.net/v2/jpg/08/14/62/33/1000_F_814623317_VHpdjqAUzqxM6v86KsQhi7xa2aEVAHVG.jpg",
             "match": "0%",
             "reason": "",
@@ -361,7 +365,7 @@ def student_list():
             "grade": "修士1",
             "industry": "飲食",
             "twitter_account": "@wanko_lover",
-            "summary": "少し怠惰な面があり、自分に甘いです。協調性は高く、人に好かれているので、友達が多いです。",
+            "summary": "この学生は、友人との時間を大切にし、社交的で楽しいことを優先する傾向がある一方で、自己管理や時間管理が苦手で、やる気が出ない日が多いようです。甘やかすことも大切にしつつ、友達への感謝の気持ちも持っているが、時折約束を忘れてしまうことがあることが伺えます。また、のんびりしすぎて、重要な準備や食事を怠ることもありますが、それを気にせず楽しむ姿勢を持っています。",
             "image": "https://as2.ftcdn.net/v2/jpg/07/68/95/75/1000_F_768957537_8Jk8wDHo9pzEknXBC9mmuUBCRSdon1TX.jpg",
             "match": "0%",
             "reason": "",
@@ -372,7 +376,7 @@ def student_list():
             "grade": "修士1",
             "industry": "IT",
             "twitter_account": "@IT_teppei",
-            "summary": "普段からエンジニアとして研鑽を怠らず、ベストを尽くします。自分の理想に向かって突き進みます。普段はエンジニアとしてインターンをしており、他のエンジニアとのコミュニケーションを大切にしています。",
+            "summary": "この学生は、技術学習に対する強い意欲と向上心を持ち、自身の成長を重視しています。失敗から学び、集中力を大切にしながら積極的に挑戦しています。また、新しいツールや技術に対して柔軟に取り組む姿勢があり、困難に直面しても前向きに乗り越えようとしています。目標に向かって全力で努力し、自分の成長が会社に貢献することを信じて日々励んでいます。",
             "image": "https://as2.ftcdn.net/v2/jpg/07/68/95/75/1000_F_768957539_6BLIzLFo0ytpuYEnT4pQxk95GW0f4Psr.jpg",
             "match": "0%",
             "reason": "",
@@ -383,22 +387,29 @@ def student_list():
             "grade": "学部3",
             "industry": "コンサル",
             "twitter_account": "@consultant_kotaro",
-            "summary": "性格/n前向きで親しみやすい: 結婚や日常の出来事を喜んで共有し、ポジティブな姿勢を見せています。お祝いに対して感謝の気持ちを示し、結婚祝いのリストも公開するなど、オープンでフレンドリーな一面が見えます。/n社交的: 友人やフォロワーとの交流を大切にし、日常生活や特別な出来事を積極的にシェアしています。/n実直で細かい: 日々の出来事を詳細に報告し、誠実な対応をしている印象があります。/n活動/n学術活動: NAIST渡辺研の博士課程に在籍しており、学位取得に向けて努力しています。言語処理学会の若手支援事業に関与し、関連イベントにも参加しています。/n個人的な生活: 結婚し、家庭を築くことを大切にしており、結婚関連の情報や祝いのリストを公開するなど、プライベートな部分も積極的に共有しています。/n旅行や日常の出来事: 旅行や外出に関する情報も共有し、特に旅行先での経験を詳細に報告しています（例: ホテルチェックインやバス乗り場の情報など）。",
+            "summary": "この学生は結婚したばかりで、結婚生活を楽しみながらもさまざまな挑戦に取り組んでいます。イベントへの参加や友人との交流を大切にし、研究活動やプロジェクトにも真剣に取り組んでいます。また、周囲の人々からのサポートや祝福に感謝の気持ちを示しており、前向きな姿勢が感じられます。全体として、社会的なつながりを重視しつつ、学問に対して情熱を持っている学生と言えます。",
             "image": "https://as2.ftcdn.net/v2/jpg/07/68/95/75/1000_F_768957539_6BLIzLFo0ytpuYEnT4pQxk95GW0f4Psr.jpg",
             "match": "0%",
             "reason": "",
         },
-    ]  # 101,102,103,104
+    ]
 
     # DBからユーザー101,102,103,104のツイートを取得し、要約を作成
     for i, student in enumerate(student_data):
-        student["summary"] = summarize_student(101 + i)
-        print(student["summary"])
+        # student["summary"] = summarize_student(101 + i)
+        # print(student["summary"])
+        # DBにはすでに要約が保存されていると仮定(登録時に処理を行う)
+        pass
 
-    for student in student_data:
-        match_rate, reason = get_match_rate(student["summary"], student["industry"])
+    rates_for_students = get_match_rate(student_data)
+    for i, student in enumerate(student_data):
+        match_rate, reason = (
+            rates_for_students[f"match_rate_{i}"],
+            rates_for_students[f"reason_{i}"],
+        )
         student["match"] = match_rate
         student["reason"] = reason
+        print(student)
 
     return render_template("student_list.html", student_data=student_data)
 
@@ -441,17 +452,35 @@ def sort_scouts(scouts_list):
     return scouts_list
 
 
-def get_match_rate(summary, industory):
-    our_culture = "私たちの使命「新しい当たり前を作り続ける」新しい当たり前を作り続けるVision私たちの目指すべき姿「世界一の企業へ」Values私たちの共有価値観「やりたいことをできているか？」やっていることは興味があることなのか自己学習したくなるほどか仕事が待ち遠しいと感じたことはあるか仕事をしていてワクワクするか仕事を通して自身の成長を感じられるか「やるべきことをしているか？」仕事仲間から必要とされているか問題解決となることに取り組んでいるか事業としてスケールする仕事をしているか今できることより一歩上の目標にトライしているか今すべきことを行っているか「いい仲間に囲まれているか？」尊敬できる仲間か困っていたら助けたいと思う仲間と仕事をしているか頼りになると思える仲間がいるか一緒に仕事をしていて楽しいと思える仲間か相談できる仲間がいるか「たいせつな人をたいせつにできているか？」たいせつな人に喜ばれているかたいせつな人と一緒にいる時間を確保できているかたいせつな人ときちんと会話をしているかたいせつな人が充実した日々をおくれているか笑顔で過ごせているかベストを尽くしているか？「ベストを尽くしているか？」どんな状況でも最後まで諦めていないか最善の方法をつねに考え抜いているか細部にまでこだわって仕事をできているか上記のことを行動し続けているか未来を見据えて仕事をしているか"
+def get_match_rate(student_data):
+    our_culture = "私たちの使命:「新しい当たり前を作り続ける」Vision(私たちの目指すべき姿):\
+    「世界一の企業へ」Values(私たちの共有価値観):「やりたいことをできているか？」やっていることは\
+        興味があることなのか自己学習したくなるほどか仕事が待ち遠しいと感じたことはあるか仕事をしていて\
+            ワクワクするか仕事を通して自身の成長を感じられるか「やるべきことをしているか？」仕事仲間から\
+                必要とされているか問題解決となることに取り組んでいるか事業としてスケールする仕事をしているか\
+                    今できることより一歩上の目標にトライしているか今すべきことを行っているか「いい仲間に囲まれているか？」\
+                        尊敬できる仲間か困っていたら助けたいと思う仲間と仕事をしているか頼りになると思える仲間がいるか\
+                            一緒に仕事をしていて楽しいと思える仲間か相談できる仲間がいるか「たいせつな人をたいせつに\
+                                できているか？」たいせつな人に喜ばれているかたいせつな人と一緒にいる時間を確保できているか\
+                                    たいせつな人ときちんと会話をしているかたいせつな人が充実した日々をおくれているか笑顔で\
+                                        過ごせているかベストを尽くしているか？「ベストを尽くしているか？」\
+                                            どんな状況でも最後まで諦めていないか最善の方法をつねに考え抜いているか\
+                                                細部にまでこだわって仕事をできているか上記のことを行動し続けているか未来を見据えて仕事をしているか"
+    msg_content = ""
+    for i, student in enumerate(student_data):
+        msg_content += (
+            student["summary"]
+            + "\nこの学生が私たちレアゾンの企業文化に合うかどうかを判定してください。以下がレアゾンの企業の文化です。json形式で、match_rate_{i}(int)とreason_{i}(string)を返してください。match_rateには0から100までの整数が入ります。match_rateが高い場合はその学生が合う理由を、低い場合はその学生が合わない理由をレアゾンのカルチャーを引用しつつ教えてください。業界とスキルと人間性がマッチするならマッチ度を90%以上にして"
+            + our_culture
+            + f"私たちの企業名はレアゾンという、ゲームや広告、フードテックなど幅広い事業を手がけて海外にも拠点を置くIT企業です。学生の志望業界は{student['industry']}です。"
+        )
+
+    msg_content += "jsonの形式は、{'match_rate_0: int, reason_0: string, match_rate_1: int, reason_1: string, match_rate_2: int, reason_2: string, match_rate_3: int, reason_3: string'}です。マッチ度の理由には、「学生の志望業界の一致度と、学生の性格とレアゾンのカルチャーの一致度の両方から説明して」"
 
     msg = [
         {
             "role": "user",
-            "content": ""
-            + summary
-            + "\nこの学生が私たちレアゾンの企業文化に合うかどうかを判定してください。以下がレアゾンの企業の文化です。json形式で、match_rate(int)とreason(string)を返してください。match_rateには0から100までの整数が入ります。match_rateが高い場合はその学生が合う理由を、低い場合はその学生が合わない理由をレアゾンのカルチャーを引用しつつ教えてください。業界とスキルと人間性がマッチするならマッチ度を90%以上にして"
-            + our_culture
-            + f"私たちの企業名はレアゾンという、ゲームや広告、フードテックなど幅広い事業を手がけて海外にも拠点を置くIT企業です。学生の志望業界は{industory}です。",
+            "content": msg_content,
         }
     ]
 
@@ -461,7 +490,9 @@ def get_match_rate(summary, industory):
         response_format={"type": "json_object"},
     )
     decoded_json = json.loads(response.choices[0].message.content)
-    return decoded_json["match_rate"], decoded_json["reason"]
+    print(decoded_json)
+    print(len(student_data))
+    return decoded_json
 
 
 def summarize_student(id):
